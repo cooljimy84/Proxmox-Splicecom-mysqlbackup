c2b56686d696bfad4ad7e5a2795a27dab67b5f4b
James Crook <cooljimy@hotmail.com>
2017-07-28 09:25:18 +0100

    Error catching didn't work as expected.

--- ContainerPart.sh
+++ ContainerPart.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
-
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 #Lets setup our objects so we can change things here, rather than in the scipt
 DB=TC_splicecom
-DUMP_FILE=/root/BackupScript/Backup.sql.gz
-THIS_SCRIPT=/root/BackupScript/ContainerPart.sh
+DUMP_FILE=`pwd`/Backup.sql.gz
 DB_USERNAME=root
 DB_PASSWORD=root
 
 # Lock the database and sleep in the background task
-mysql -u$DB_USERNAME -p$DB_PASSWORD $DB -e "FLUSH TABLES WITH READ LOCK; DO SLEEP(3600);" &
-sleep 3
+#Lock the database and sleep in the background task
+if mysql --version; then
+	#Keep going we have mysql installed
+	mysql -u$DB_USERNAME -p$DB_PASSWORD $DB -e "FLUSH TABLES WITH READ LOCK; DO SLEEP(3600);" &
+	sleep 3
 
-# Export and compress the database while it is still locked
-mysqldump --hex-blob --skip-comments -u$DB_USERNAME -p$DB_PASSWORD $DB | gzip > $DUMP_FILE
+	#Export and compress the database while it is still locked
+	mysqldump --hex-blob --skip-comments -u$DB_USERNAME -p$DB_PASSWORD $DB | gzip > $DUMP_FILE
 
-# When finished, kill the previous background task to unlock
-kill $! 2>/dev/null
-wait $! 2>/dev/null
+	#When finished, kill the previous background task to unlock
+	kill $! 2>/dev/null
+	wait $! 2>/dev/null
+	exit 0
+else
+	echo "!!! Failed mysql command exiting !!!"
+	exit 1
+fi
--- HostPart.sh
+++ HostPart.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 # Setup time
 ACTIVECONTAINERS=ActiveContainers.list
-LOCATION=/root/Proxmox-Splicecom-mysqlbackup-master
+LOCATION=`pwd`
 
 # List active containers to
 lxc-ls --active -1 > $ACTIVECONTAINERS
 
 # Erm
-#while read -r line
 for line in $(cat $ACTIVECONTAINERS)
 do
-        name="$line"
-        echo "#########################################################"
-        echo "Container number $name"
-        echo "Lets create the directory for the script"
-        lxc-attach -n $name -- mkdir /root/BackupScript/
-        echo "Lets push the files we need to the container"
-        pct push $name $LOCATION/ContainerPart.sh /root/BackupScript/ContainerPart.sh
-        echo "chmod +x so we can run the bloodly thing"
-        lxc-attach -n $name -- chmod +x /root/BackupScript/ContainerPart.sh
-        echo "Attach to the container to run the script we just pushed"
-        lxc-attach -n $name -- /root/BackupScript/./ContainerPart.sh
-	done || exit 1
-        echo "Pull the Backup.sql.gz file and change its name to the container name"
-        pct pull $name /root/BackupScript/Backup.sql.gz $LOCATION/$name-`date +"%d%m%Y"`.sql.gz
-	done || exit 1
+    name="$line"
+    echo "#########################################################"
+    echo "Container number $name"
+    echo "Lets create the directory for the script"
+    lxc-attach -n $name -- mkdir /root/BackupScript/
+    echo "Lets push the files we need to the container"
+    pct push $name $LOCATION/ContainerPart.sh /root/BackupScript/ContainerPart.sh
+    echo "chmod +x so we can run the bloodly thing"
+    lxc-attach -n $name -- chmod +x /root/BackupScript/ContainerPart.sh
+	echo "Attach to the container to run the script we just pushed"
+    lxc-attach -n $name -- /root/BackupScript/./ContainerPart.sh
+	echo "Pull the Backup.sql.gz file and change its name to the container name"
+	pct pull $name /root/BackupScript/Backup.sql.gz $name-`date +"%d%m%Y"`.sql.gz
 	echo "Attach to the container to and remove script and backup file"
 	lxc-attach -n $name -- rm -f /root/BackupScript/ContainerPart.sh
-	done || exit 1
 	lxc-attach -n $name -- rm -f /root/BackupScript/Backup.sql.gz
-	done || exit 1
+	echo "Next"
 done
-#done < "$ACTIVECONTAINERS"
 
 # Cleanup our active containers.list
 rm -f $ACTIVECONTAINERS
